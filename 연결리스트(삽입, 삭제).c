60cf59e055efd583cd8fde1a23cd1ecda3fe58ca
송현규 <108565747+HyunKyu-Song@users.noreply.github.com>
2022-11-29 16:59:21 +0900

    C언어

---
+++ 연결리스트(삽입, 삭제).c
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct linkedlist{
+	int data;
+	struct linkedlist * next;
+}LinkedList;
+
+void init(LinkedList *head, LinkedList *tail)
+{
+	head->next = NULL;
+}
+
+void Insert(LinkedList *head, LinkedList *tail, int value)
+{
+	LinkedList *newnode;
+	
+	newnode = (LinkedList*)malloc(sizeof(newnode));
+	newnode->data = value;
+	newnode->next = NULL;
+	
+	if(head->next == NULL){
+		head->next = newnode;
+		tail->next = newnode;
+	}
+	else{
+		tail->next->next = newnode;
+		tail->next = newnode;
+	}
+}
+
+void Delete(LinkedList *head, LinkedList *tail, int value)
+{
+	LinkedList *p, *rem;
+	
+	rem = head->next;
+	p = NULL;
+	
+	while(rem->data != value){
+		p = rem;
+		rem = rem->next;
+	}
+	
+	if(p == NULL){
+		head->next = head->next->next;
+	}
+	else{
+		p->next = rem->next;
+		tail->next = p;
+	}
+	
+	free(rem);
+}
+
+void Print(LinkedList *head)
+{
+	LinkedList *copy;
+	
+	copy = (LinkedList*)malloc(sizeof(copy));
+	
+	copy->next = head->next;
+	
+	printf("\n저장된 데이터: \n");
+	while(copy->next != NULL){
+		printf("%d ", copy->next->data);
+		copy->next = copy->next->next;
+	}
+}
+
+int main(void){
+	
+	LinkedList head, tail;
+	int n, data;
+	
+	init(&head, &tail);
+	
+	while(1){
+		printf("\n\n[1] Insert\n[2] Delete\n[3] Print\n[4] Exit\n\n");
+		scanf("%d", &n);
+		
+		switch(n){
+			case 1:
+				printf("저장할 데이터: ");
+				scanf("%d", &data);
+				Insert(&head, &tail, data);
+				break;
+			case 2:
+				printf("삭제 할 데이터: ");
+				scanf("%d", &data);
+				Delete(&head, &tail, data);
+				break;
+			case 3:
+				Print(&head);
+				break;
+			case 4:
+				printf("\n-----------------------------\n\n");
+				printf(" 프로그램이 종료되었습니다.\n\n");
+				printf("-----------------------------");
+				exit(-1);
+		}
+	}
+	
+	return 0;
+}
--- 연습.c
+++ 연습.c
@@ -1,32 +1,281 @@
+/*
+1
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-typedef struct std{
-	int num;
-	char name[10];
-	float grade;
-}STD;
+typedef struct linkedlist{
+	int data;
+	struct linkedlist * next;
+}LinkedList;
+
+void init(LinkedList *head, LinkedList *tail)
+{
+	head->next = NULL;
+	tail->next = head;
+}
+
+void Insert(LinkedList *head, LinkedList *tail, int n)
+{
+	LinkedList *newnode;
+	
+	newnode = (LinkedList*)malloc(sizeof(newnode));
+	newnode->data = n;
+	newnode->next = NULL;
+	
+	if(head->next == NULL){
+		head->next = newnode;
+		tail->next = newnode;
+	}
+	else{
+		tail->next->next = newnode;
+		tail->next = newnode;
+	}
+}
+
+void Print(LinkedList *head)
+{
+	while(head->next != NULL){
+		printf("%d ", head->next->data);
+		head->next = head->next->next;
+	}
+}
 
 int main(void){
 	
-	STD s;
-	char str[10];
+	LinkedList head, tail;
+	int n;
 	
-	printf("학번: ");
-	scanf("%d", &s.num);
-	/*
-	printf("이름: ");
-	scanf("%s", &s.name);
-	*/
-	strcpy(s.name, "song");
-	//s.name = "song";
+	init(&head, &tail);
 	
-	
-	printf("학점: ");
-	scanf("%f", &s.grade);
-	
-	printf("\n\n학번: %d\n이름: %s\n학점: %.1f", s.num, s.name, s.grade);
+	while(1){
+		printf("저장할 데이터: ");
+		scanf("%d", &n);
+		
+		if(n == -1){
+			break;
+		}
+		
+		Insert(&head, &tail, n);
+	}
+	Print(&head);
 	
 	return 0;
 }
+*/
+
+/*
+2
+#include <stdio.h>
+#define SIZE 8
+
+void sort(int arr[], int n)
+{
+	int i, j, k, temp, max;
+	
+	for(i = 0; i < n-1; i++){
+		max = i;
+		for(j = i; j < n; j++){
+			if(arr[max] < arr[j])
+				max = j;
+		}
+		temp = arr[i];
+		arr[i] = arr[max];
+		arr[max] = temp;
+		for(k = 0; k < n; k++){
+			printf("%d ", arr[k]);
+		}
+		printf("\n");
+	}
+}
+
+int main(void){
+	
+	int arr[SIZE] = {2, 8, 7, 4, 3, 1, 9, 5};
+	int i;
+	
+	printf("[ 정렬 전]\n");
+	for(i = 0; i < SIZE; i++){
+		printf("%d ", arr[i]);
+	}
+	printf("\n\n");
+	
+	sort(arr, SIZE);
+	
+	printf("\n\n[ 정렬 후]\n");
+	for(i = 0; i < SIZE; i++){
+		printf("%d ", arr[i]);
+	}
+	
+	return 0;
+}
+*/
+
+/*
+3
+#include <stdio.h>
+#include <stdlib.h>
+#define SIZE 10
+
+typedef struct stack{
+	char arr[SIZE];
+	int top;
+}Stack;
+
+void Init(Stack *s)
+{
+	s->top = -1;
+}
+
+void Push(Stack *s, char data)
+{
+	s->arr[++(s->top)] = data;
+}
+
+void Pop(Stack *s)
+{
+	(s->top)--;
+}
+
+void Print(Stack *s)
+{
+	int i;
+	
+	printf("\n\n[ 출력결과 ]\n");
+	for(i = 0; i <= s->top; i++){
+		printf("%c ", s->arr[i]);
+	}
+	printf("\n\n");
+}
+
+int main(void){
+	
+	Stack s;
+	int n;
+	char data;
+	
+	Init(&s);
+	
+	while(1){
+		printf("[1] Push [2] Pop [3] Exit\n\n");
+		scanf("%d", &n);
+		
+		switch(n){
+			case 1:
+				printf("data: ");
+				scanf(" %c", &data);
+				Push(&s, data);
+				Print(&s);
+				break;
+			case 2:
+				Pop(&s);
+				Print(&s);
+				break;
+			case 3:
+				exit(-1);
+		}
+	}
+	
+	return 0;
+}
+*/
+
+/*
+#include <stdio.h>
+#include <stdlib.h>
+#define SIZE 10
+
+typedef struct queue{
+	char arr[SIZE];
+	int front, rear;
+}Queue;
+
+void Init(Queue *q)
+{
+	q->front = -1;
+	q->rear = -1;
+}
+
+void Push(Queue *q, char data)
+{
+	q->arr[++(q->rear)] = data;
+}
+
+void Pop(Queue *q)
+{
+	q->arr[++(q->front)];
+}
+
+void Print(Queue *q)
+{
+	int i;
+	
+	printf("\n\n[ 출력결과 ]\n");
+	for(i = (q->front)+1; i <= q->rear; i++){
+		printf("%c ", q->arr[i]);
+	}
+	printf("\n\n");
+}
+
+int main(void){
+	
+	Queue q;
+	int n;
+	char data;
+	
+	Init(&q);
+	
+	while(1){
+		printf("[1] Push [2] Pop [3] Exit\n\n");
+		scanf("%d", &n);
+		
+		switch(n){
+			case 1:
+				printf("data: ");
+				scanf(" %c", &data);
+				Push(&q, data);
+				Print(&q);
+				break;
+			case 2:
+				Pop(&q);
+				Print(&q);
+				break;
+			case 3:
+				exit(-1);
+		}
+	}
+	
+	return 0;
+}
+*/
+
+/*
+8
+#include <stdio.h>
+#define SIZE 15
+
+int main(void){
+	
+	int i, k;
+	int cnt=0;
+	int arr[SIZE] = {0, };
+	
+	for(i = 0; i < SIZE; i++){
+		printf("k의 값을 입력: ");
+		scanf("%d", &k);
+		
+		arr[i] = k;
+		
+		if(k % 3 == 0){
+			cnt++;
+		}
+	}
+	
+	printf("\n\n[ 전체원소 출력 ]\n");
+	for(i = 0; i < SIZE; i++){
+		printf("%d ", arr[i]);
+	}
+	printf("\n\ncnt = %d", cnt);
+	
+	return 0;
+}
+*/
